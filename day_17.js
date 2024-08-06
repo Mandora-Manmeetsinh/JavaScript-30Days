892b347e93beec5cf52995b290a7ad740fcecd9e
Mandora Manmeetsinh Dashrathsinh <157201046+Mandora-Manmeetsinh@users.noreply.github.com>
2024-08-06 16:32:13 +0530

    Add files via upload

---
+++ day_17.js
@@ -0,0 +1,71 @@
+/* Question 17 from day 17
+
+Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
+The class has three public methods:
+set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds. Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
+get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+count(): returns the count of un-expired keys.
+
+Example 1:
+Input: 
+actions = ["TimeLimitedCache", "set", "get", "count", "get"]
+values = [[], [1, 42, 100], [1], [], [1]]
+timeDelays = [0, 0, 50, 50, 150]
+Output: [null, false, 42, 1, -1]
+
+Example 2:
+
+Input: 
+actions = ["TimeLimitedCache", "set", "set", "get", "get", "get", "count"]
+values = [[], [1, 42, 50], [1, 50, 100], [1], [1], [1], []]
+timeDelays = [0, 0, 40, 50, 120, 200, 250]
+Output: [null, false, true, 50, 50, -1, 0] */
+
+var TimeLimitedCache = function() {
+    this.cache = {};
+};
+
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+  if (this.cache[key] && this.cache[key].timer) {
+    clearTimeout(this.cache[key].timer);
+    this.cache[key].value = value;
+    this.cache[key].timer = setTimeout(() => {
+      this.remove(key);
+    }, duration);
+    return true;
+  } else {
+    this.cache[key] = {
+      value: value,
+      timer: setTimeout(() => {
+        this.remove(key);
+      }, duration)
+    };
+    return false;
+  }
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+  if (this.cache[key] && this.cache[key].timer) {
+    return this.cache[key].value;
+  } else {
+    return -1;
+  }
+};
+
+TimeLimitedCache.prototype.count = function() {
+  let count = 0;
+  for (const key in this.cache) {
+    if (this.cache[key].timer) {
+      count++;
+    }
+  }
+  return count;
+};
+
+TimeLimitedCache.prototype.remove = function(key) {
+  delete this.cache[key];
+};
